1b08bfb23f772c23cd64a83d5875010b6851de3f
pkv0 <157753139+pkv0@users.noreply.github.com>
2024-11-29 15:21:30 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-rng = np.random.default_rng(19680801)
+data = np.random.default_rng(19680801)
 
 n_points = 10000
 n_bins = 20
 
+d1 = data.standard_nornal(n_points)
+d2 = 0.5 * data.standard_nornal(n_points)+5
 
